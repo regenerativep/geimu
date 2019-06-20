4eb19c4f848cb595d93be3d6635a0502dfcfe349
regenerativep <regeneralp@gmail.com>
2019-06-20 17:13:13 -0400

    objects

---
+++ Geimu/Geimu/GameObject.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geimu
+{
+    public abstract class GameObject
+    {
+        public Vector2 Position { get; set; }
+        public Vector2 Velocity { get; set; }
+        public Vector2 Size { get; set; }
+        public GameObject(Vector2 pos, Vector2 vel, Vector2 size)
+        {
+            Position = pos;
+            Velocity = vel;
+            Size = size;
+        }
+    }
+}
---
+++ Geimu/Geimu/ReimuObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Geimu
+{
+    public class ReimuObject : GameObject
+    {
+        public ReimuObject(Vector2 pos, Vector2 vel, Vector2 size) : base(pos, vel, size)
+        {
+
+        }
+    }
+}
