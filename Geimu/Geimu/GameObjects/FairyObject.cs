b3cf5332edb3ab46728a9856a7e4fd1300b91dc2
gabman15 <32818424+gabman15@users.noreply.github.com>
2019-06-21 20:40:31 -0400

    added fairy

--- Geimu/Geimu/GameObjects/FairyObject.cs
+++ Geimu/Geimu/GameObjects/FairyObject.cs
@@ -7,18 +7,19 @@ using System.Threading.Tasks;
 
 namespace Geimu.GameObjects
 {
-    class EnemyObject : GameObject
+    public abstract class FairyObject : GameObject
     {
         public static float MoveSpeed = 1.5f;
-        //public static float JumpSpeed = -6;
         public static float HorizontalFriction = 1;
         public static Vector2 MaxVelocity = new Vector2(4, 16);
         public static float Gravity = 0.3f;
         public static float IdleMaxSpeed = 3;
 
-        public EnemyObject(Room room, Vector2 pos) : base(room, pos, new Vector2(0, 0), new Vector2(128, 128))
-        {
+        private bool facingRight;
 
+        public FairyObject(Room room, Vector2 pos) : base(room, pos, new Vector2(0, 0), new Vector2(64, 64))
+        {
+            facingRight = true;
         }
     }
 }
